4a4199ce486e90d47c7b88049ef0a842534796f8
codedream <codedreamfy@outlook.com>
2017-04-01 01:07:25 +0800

    navg

--- fe/src/components/nav.vue
+++ fe/src/components/nav.vue
@@ -7,28 +7,24 @@
       <Menu mode="horizontal" width="auto" :theme="theme1" active-name="1" class="navg-list">
         <Menu-item name="1">
           <Icon type="ios-paper"></Icon>
-          内容管理
+          首页
         </Menu-item>
         <Menu-item name="2">
           <Icon type="ios-people"></Icon>
-          用户管理
+          前端开发
         </Menu-item>
-        <Submenu name="3">
-          <template slot="title">
-            <Icon type="stats-bars"></Icon>
-            统计分析
-          </template>
-          <Menu-group title="使用">
-            <Menu-item name="3-1">新增和启动</Menu-item>
-            <Menu-item name="3-2">活跃分析</Menu-item>
-            <Menu-item name="3-3">时段分析</Menu-item>
-          </Menu-group>
-          <Menu-group title="留存">
-            <Menu-item name="3-4">用户留存</Menu-item>
-            <Menu-item name="3-5">流失用户</Menu-item>
-          </Menu-group>
-        </Submenu>
-
+        <Menu-item name="3">
+          <Icon type="ios-people"></Icon>
+          杂文摘要
+        </Menu-item>
+        <Menu-item name="4">
+          <Icon type="ios-people"></Icon>
+          电影推荐
+        </Menu-item>
+        <Menu-item name="5">
+          <Icon type="ios-people"></Icon>
+          音乐推荐
+        </Menu-item>
       </Menu>
     </Col>
   </Row>
